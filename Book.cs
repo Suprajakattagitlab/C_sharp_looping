f2bd8bf68e497c297bad025a5fd1bb4c2391da85
SuprajaKatta9 <supraja.katta@klingelnberg.com>
2023-01-19 15:49:26 +0530

    committing looping prgms

---
+++ Book.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp__looping
+{
+    class Book
+    {
+        List<int> price = new List<int>();
+        int bill = 0;
+        public  void AddBookPrice(int p)
+        {
+            price.Add(p);
+            
+        }
+        public int TotalBill()
+        {
+            foreach (int p in price) 
+            {
+                bill = p + bill;
+            }
+            return bill;  
+        }
+        public char Badge(int bill)
+        {
+            switch (bill)
+            {
+                case var l when l > 500:
+                    return 'A';
+
+                case var l when l > 200:
+                    return 'B';
+
+                case var l when l > 100:
+                    return 'C';
+                default:
+                    return 'D';
+            }
+        }
+            public void Badge(char A)
+            {
+                switch (A)
+                {
+                    case'A':
+                        Console.WriteLine("platinum user");
+                        break;
+                    case 'B':
+                        Console.WriteLine("Golden user");
+                        break;
+                    case 'C':
+                        Console.WriteLine("Silver user");
+                        break;
+                    default:
+                        Console.WriteLine("Normal user");
+                        break;
+                }
+
+            } 
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using c_sharp__looping;
+using System;
+namespace c_sharp_looping
+{
+    public class loop
+    {
+        public static void Main(String[] args)
+        {
+            Book b = new Book();
+            Boolean f = true;
+            while (f != false)
+            {
+                Console.WriteLine("Enter the grades or enter 0 if wants to quite");
+
+                var input = Console.ReadLine();
+                var pr = int.Parse(input);
+                if (pr == 0)
+                {
+                    break;
+                }
+
+                b.AddBookPrice(pr);
+            }
+            int re = b.TotalBill();
+            Console.WriteLine(re);
+            char c=b.Badge(re);
+            Console.WriteLine(c);
+
+            b.Badge(c);
+
+
+
+
+            
+
+
+        }
+       
+            
+
+    }
+        
+}
